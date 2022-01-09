002b75a5dd4816ffeef806c4eb1c385c6ad88a20
Hongjia Liu <hongjia_liu@outlook.com>
2022-01-09 01:05:33 +0000

    Setup React Router 6 and update AboutPage component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {v4 as uuidv4} from 'uuid'
 import { useState } from "react";
 import Header from "./components/Header";
@@ -5,6 +6,7 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
 
 const App = () => {
 
@@ -21,13 +23,23 @@ const App = () => {
 
     
     return ( 
-        <>
-            <Header />
-            <div className="container">
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-            </div>
+        <>  
+            <Router>
+                <Header /> 
+                <div className="container">
+                    <Routes>
+                        <Route exact path='/' element={
+                            <>
+                                <FeedbackForm handleAdd={addFeedback} />
+                                <FeedbackStats feedback={feedback} />
+                                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                            </>
+                        } />
+
+                        <Route path='/about' element={<AboutPage />} />
+                    </Routes>
+                </div>
+            </Router>
         </>
      );
 }
