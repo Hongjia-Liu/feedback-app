487769dd40b78e81bbd3b121db9ab57f71b75baf
Hongjia Liu <hongjia_liu@outlook.com>
2022-02-21 00:32:17 +0000

    Add feedback to JSON-Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
@@ -27,9 +26,18 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// Add feedback
-	const addFeedback = newFeedback => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
+	const addFeedback = async newFeedback => {
+		const response = await fetch(`http://localhost:5000/feedback`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newFeedback),
+		});
+
+		const data = await response.json();
+
+		setFeedback([data, ...feedback]);
 	};
 
 	// Delete feedback
