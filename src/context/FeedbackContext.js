5b59b84521aa0c55fc6609b2ff4ce7362e1b039c
Hongjia Liu <hongjia_liu@outlook.com>
2022-02-22 01:47:59 +0000

    Delete and Update feedback from JSON-Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -41,16 +41,27 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// Delete feedback
-	const deleteFeedback = feedbackItem => {
+	const deleteFeedback = async feedbackItem => {
+		await fetch(`http://localhost:5000/feedback/${feedbackItem.id}`, {
+			method: "DELETE",
+		});
 		setFeedback(feedback.filter(item => item.id !== feedbackItem.id));
 	};
 
 	// Update feedback item
-	const updateFeedback = (id, updItem) => {
+	const updateFeedback = async (id, updItem) => {
+		const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updItem),
+		});
+
+		const data = await response.json();
+
 		setFeedback(
-			feedback.map(item =>
-				item.id === id ? { ...item, ...updItem } : item
-			)
+			feedback.map(item => (item.id === id ? { ...item, ...data } : item))
 		);
 	};
 
